d3a829835c9a183aa18a230af51081b798d97767
shparutask <sofish7184@gmail.com>
2017-03-02 11:49:06 +0300

    Modified 2.2

--- ДЗ_2/Ex_2/Ex_2/Ex_2.cpp
+++ ДЗ_2/Ex_2/Ex_2/Ex_2.cpp
@@ -6,11 +6,11 @@ int main()
 	int* a[10];
 
 	for (int i = 0; i < 10; i++) {
-		a[i] = new int[i];
+		a[i] = new int[i + 1];
 		for (int j = 0; j < i - 1; j++) {
 			a[i][j] = 0;
 		}
-		a[i][i - 1] = 1;
+		a[i][i] = 1;
 
 		for (int j = 0; j < i; j++)
 		{

689cfa194e7b90ddc0ec7d923fdc614ffc4af62c
shparutask <sofish7184@gmail.com>
2017-03-05 00:17:39 +0300

    Modified 2 and 3 exercises

--- ДЗ_3/Ex_1/Ex_1/main.cpp
+++ ДЗ_3/Ex_1/Ex_1/main.cpp
@@ -2,36 +2,48 @@
 using namespace std;
 
 class stack {
-	int a[100];
-
 public:
 	stack();
 	void push(int new_element);
 	int pop();
-	int max();
+	int& max();
 
 private:
+	int* a;
 	int* p;
-	int length;
+	int size;
+	int count;
 };
 
 stack::stack() {
+	a = new int[100];
 	p = a;
-	length = 3;
+	size = 100;
+	count = 0;
 }
 
 void stack::push(int new_element) {
+	count++;
+	if (count >= 100) {
+		size *= 2;
+		int* a1 = new int[size];
+		for (int i = 0; i < count; i++) {
+			a1[i] = a[i];
+		}
+		a = a1;
+		delete[] a1;
+	}
 	p++;
-	*p = new_element;
+	a[count] = new_element;
 }
 
-int stack::max() {
-	int max = a[0];
+int& stack::max() {
+	int* max = a;
 	for (int* k = a; k < p; k++)
 	{
-		if (*k >= max) max = *k;
+		if (*k >= *max) max = k;
 	}
-	return max;
+	return *max;
 }
 
 int stack::pop()
@@ -48,6 +60,7 @@ int main() {
 	s.push(3);
 	s.push(7);
 	cout << s.pop();
+	s.max()++;
 	cout << " " << s.max();
 	system("pause");
 	return 0;

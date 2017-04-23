6fa17fdf9a4a8900b9e4f844a2dd7160b36b0a78
shparutask <sofish7184@gmail.com>
2017-04-23 15:37:08 +0300

    Modified HW

--- ДЗ_10/Ex_4/Ex_4/Ex_4.cpp
+++ ДЗ_10/Ex_4/Ex_4/Ex_4.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main() {
 	double x;
 	cin >> x;
-	double* px = &x;
-	auto p = reinterpret_cast<char*>(px);
-	char* px1 = new char(4);
-	char* px2 = new char(4);
-	for (int i = 0; i < 4; i++) {		px1[i] = px[i];		px2[i] = px[i + 4];	}	int x1 = reinterpret_cast<int>(px1);	int x2 = reinterpret_cast<int>(px2);	cout << x1 << endl;	cout << x2 << endl;	system("pause");	return 0;}
+	auto p = reinterpret_cast<int*>(&x);
+	int px1 = p[1];
+	int px2 = p[2];
+	cout << px1 << endl;
+	cout << px2 << endl;
+
+	system("pause");
+	return 0;
+}
 

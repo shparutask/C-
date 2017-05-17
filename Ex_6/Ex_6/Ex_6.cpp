4fb7b585c4e381e3e01e29a0e7cb8d3e45e6aeae
shparutask <sofish7184@gmail.com>
2017-05-17 11:38:47 +0300

    CW

---
+++ Ex_6/Ex_6/Ex_6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+auto sum(T b, T e)
+{
+	decltype(*b) sum = 0;
+
+	int count = 0;
+	for (T p = b; p < e; p++)
+		count++;
+	if (count % 2 == 0) e--;
+
+	for (T p = b; p < e; p += 2)
+		sum += *p;
+	return sum;
+}
+
+int main() {
+	vector<int> x{ 3, 4, 6, 6, 7, 8 ,34, 5 };
+	cout << sum(x.begin(), x.end());
+	system("pause");
+	return 0;
+}
